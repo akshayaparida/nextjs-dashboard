cd067525b62b1a9e4f2dd6eacaef82826c62e552
Akshaya Parida <akshayaparida2811@gmail.com>
2024-10-19 00:23:30 +0530

    added all files

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
- 
+/** /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    experimental: {
-      ppr: 'incremental',
-    },
-  };
-   
-  export default nextConfig;
+  experimental: {
+    // Add any valid experimental features here
+    incrementalCacheHandlerPath: true, // Example of valid experimental feature
+  },
+  // You can also include other Next.js configurations here
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig;
